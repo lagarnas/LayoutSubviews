ab2518696d74bc83b202881bf1916c008d476ca8
lagarnas <lagarnas@gmail.com>
2020-11-20 10:10:54 +0400

    Initial Commit

---
+++ LayoutSubviews vc layoutIfNeeded/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LayoutSubviews vc layoutIfNeeded
+//
+//  Created by Анастасия Леонтьева on 20.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
